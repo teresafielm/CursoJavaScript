bdc2b8916f764f0067389c0e6e86372eda6b8f8c
Teresa Fiel Muñoz <37962089+teresafielm@users.noreply.github.com>
2023-01-23 13:17:30 -0600

    Add files via upload

---
+++ holamundo/PrimerEjemplo.js
@@ -0,0 +1,4 @@
+var json = '{"nombre":"Zuriel","edad":23,"nacionalidad":"Mexicana","hobbies":["ajedrez","video juegos","comics"]}';
+var obj = JSON.parse(json);
+console.log(obj.hobbies[0]);
+
---
+++ holamundo/SegundoEjemplo.js
@@ -0,0 +1,42 @@
+//function suma(a, b){
+
+//    return a +b;
+
+//}
+
+//var suma=function(a,b){
+//    return a+b;
+//}
+
+
+//datos propiedades y captura de pantalla
+//let operacion={
+ //   a:50,
+ //   b:40,
+ //   suma:function(){
+  //      return this.a*this.b;
+  //  },
+  //  resta:function(){
+   //     return this.a-this.b;
+   // }
+//}
+
+//var resultado = operacion.resta() ;
+//console.log(resultado);
+
+//let suma = (a, b) =>{
+    //return a+b;
+
+//}
+
+//var resultado = suma (4,5);
+//console.log(resultado);
+
+
+//Simplificacion de amputador flecha
+let multiplicacion = (a, b) =>a*b;
+
+
+
+var resultado = multiplicacion (4,5);
+console.log(resultado);
